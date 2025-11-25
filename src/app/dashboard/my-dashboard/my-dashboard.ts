ad076800b0dfa18118639b3fbc66aab0fcc5bda9
BSquad <121628964+BSquad@users.noreply.github.com>
2025-11-25 11:00:25 +0100

    fibbonaci test

--- src/app/dashboard/my-dashboard/my-dashboard.ts
+++ src/app/dashboard/my-dashboard/my-dashboard.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-dashboard',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './my-dashboard.html',
   styleUrl: './my-dashboard.scss',
-  standalone: true, 
+  standalone: true,
 })
 export class MyDashboard {
+  n: number = 0;
+  fibonacci: number[] = [];
 
+  calculateFibonacci() {
+    this.fibonacci = [];
+    if (this.n <= 0) return;
+
+    let a = 0, b = 1;
+    for (let i = 0; i < this.n; i++) {
+      this.fibonacci.push(a);
+      const next = a + b;
+      a = b;
+      b = next;
+    }
+  }
 }
