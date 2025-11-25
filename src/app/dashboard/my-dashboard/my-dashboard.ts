c7afbb260b7a0c1c11fc3d9eaebe51dc21cebc6c
BSquad <121628964+BSquad@users.noreply.github.com>
2025-11-25 10:29:13 +0100

    dashboard component test

---
+++ src/app/dashboard/my-dashboard/my-dashboard.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-dashboard',
+  imports: [],
+  templateUrl: './my-dashboard.html',
+  styleUrl: './my-dashboard.scss',
+  standalone: true, 
+})
+export class MyDashboard {
+
+}
