7b0733e0ba1d86b4ef0d5195523f1fd311c4daf4
BSquad <121628964+BSquad@users.noreply.github.com>
2025-12-16 13:31:59 +0100

    singal fürs laden

--- src/app/pages/list/person-list/person-list.ts
+++ src/app/pages/list/person-list/person-list.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestApi } from '../../../api/rest-api';
 import { Person } from '../../../models/person';
@@ -11,13 +11,14 @@ import { Person } from '../../../models/person';
   standalone: true
 })
 export class PersonList {
-  persons: Person[] = [];
+  persons: any = signal<Person[]>([]);
 
   constructor(private restApi: RestApi) {  }
 
   async ngOnInit() {
     try {
-      this.persons = await this.restApi.loadPersonList();
+      const data = await this.restApi.loadPersonList();
+      this.persons.set(data);
     } catch (err) {
       console.error('Fehler beim Laden:', err);
     }
