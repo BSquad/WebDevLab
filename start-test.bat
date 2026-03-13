5bf40151da3178b550f2133d1e82a8c85afaa773
BSquad <121628964+BSquad@users.noreply.github.com>
2026-03-13 17:41:38 +0100

    added backend to test scripts

--- start-test.bat
+++ start-test.bat
@@ -1,1 +1,2 @@
 start cmd /k "cd frontend && ng test"
+start cmd /k "cd backend && npm run test"
--- start-test.sh
+++ start-test.sh
@@ -3,4 +3,6 @@ echo "Starte Tests..."
 
 gnome-terminal -- bash -c "cd frontend && echo 'Frontend wird gestartet...' && ng test; exec bash"
 
+gnome-terminal -- bash -c "cd backend && echo 'Backend wird gestartet...' && npm run test; exec bash"
+
 echo "Tests gestartet."
