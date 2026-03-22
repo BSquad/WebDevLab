d70f2eadb1e7cb2eb3c72016be8f0803e5a43ee7
Jobst <jobst.hillebrandt@entiac.com>
2026-03-22 12:33:32 +0100

    instrucitons for guides rdy / coverage will now be dispalyed after backendtesting

--- start-test.sh
+++ start-test.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-echo "Starte Tests..."
+echo "Starting tests..."
 
 ./reset-db.sh
 
-# open new terminal windows for the tests
-gnome-terminal -- bash -c "cd frontend && echo 'Frontend wird gestartet...' && ng test; exec bash"
+gnome-terminal -- bash -c "cd frontend && ng test --code-coverage; exec bash"
 
-gnome-terminal -- bash -c "cd backend && echo 'Backend wird gestartet...' && npm run test; exec bash"
+gnome-terminal -- bash -c "cd backend && npm run test -- --coverage --coverageReporters=text-summary; exec bash"
 
-echo "Tests gestartet."
+echo "Tests started."
