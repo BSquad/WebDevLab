f982caac81abd79b547d5c988945f356de8ef04d
Jobst <jobst.hillebrandt@entiac.com>
2026-03-22 15:19:03 +0100

    dfgashsfghjfh

--- start-project.bat
+++ start-project.bat
@@ -1,7 +1,4 @@
 @echo off
-call reset-db.bat
-
-
 
 start cmd /k "cd frontend && ng serve"
 start cmd /k "cd backend && npm run start:dev"
--- start-test.bat
+++ start-test.bat
@@ -1,3 +1,3 @@
 call reset-db.bat
 start cmd /k "cd frontend && ng test"
-start cmd /k "cd backend && npm run test -- --coverage --coverageReporters=text-summary --coverageReporters=lcov"
+start cmd /k "cd backend && npm run test -- --coverage --coverageReporters=text-summary --coverageReporters=stest"
